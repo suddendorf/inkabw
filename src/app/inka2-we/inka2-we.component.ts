fd034c1e125bfff20db07e193b570f10c2cc0293
Suddendorf <suddendorf@sddb.de>
2017-06-12 15:02:10 +0200

    admin_daten_collapse

--- src/app/inka2-we/inka2-we.component.ts
+++ src/app/inka2-we/inka2-we.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import{WE} from '../we';
+import { WE } from '../we';
 @Component({
   selector: 'app-inka2-we',
   templateUrl: './inka2-we.component.html',
   styleUrls: ['./inka2-we.component.css']
 })
 export class Inka2WeComponent implements OnInit {
-  model: WE=new WE();
-  constructor() { }
+  model: WE = new WE();
+  private title;
+  private collapse: boolean = false;
+  constructor() {
+  }
 
   ngOnInit() {
+    let lid: string = sessionStorage.getItem('title');
+    console.log(lid);
+    this.title = lid;
+  }
+  private toggleCollapse() {
+    this.collapse = !this.collapse;
   }
 
 }
